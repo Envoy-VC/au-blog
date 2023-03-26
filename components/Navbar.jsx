1eeece398f39d9d648512b06ebbca1c780d305d9
Vedant Chainani <vedantchainani1084@gmail.com>
2023-03-26 22:53:00 +0530

    Initial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,7 @@
+const Navbar = () => (
+	<nav className='bg-white flex flex-row w-full px-4 md:px-12 py-6 max-w-screen-2xl mx-auto border-2 border-red-500'>
+		<div className='flex justify-start items-center'></div>
+	</nav>
+);
+
+export default Navbar;
