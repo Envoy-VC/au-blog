32543fb2918cf1354c7f2e4de602e9a411571432
Vedant Chainani <vedantchainani1084@gmail.com>
2023-04-02 01:06:23 +0530

    Add Browse Articles Section

---
+++ components/BlogCard.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import Image from 'next/image';
+
+const BlogCard = ({ coverImage, title, brief, slug }) => {
+	return (
+		<article class='transition-all hover:scale-[103%] overflow-hidden rounded-lg border border-gray-100 shadow-sm'>
+			<Image
+				alt='Blog Card'
+				src={coverImage}
+				width={500}
+				height={224}
+				class='h-56 w-full sm:object-cover object-contain'
+			/>
+
+			<div class='p-4 sm:p-6'>
+				<a href='#'>
+					<h3 class='text-lg font-medium text-gray-900'>{title}</h3>
+				</a>
+
+				<p class='mt-2 text-sm leading-relaxed text-gray-500 line-clamp-3'>
+					{brief.slice(0, 130) + '...'}
+				</p>
+
+				<a
+					href='#'
+					class='group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600'
+				>
+					Find out more
+					<span
+						aria-hidden='true'
+						class='block transition group-hover:translate-x-0.5'
+					>
+						&rarr;
+					</span>
+				</a>
+			</div>
+		</article>
+	);
+};
+
+export default BlogCard;
---
+++ sections/BrowseArticles.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import { BlogCard } from '@/components';
+import { GET_ARTICLES } from '@/utils/query';
+
+const BrowseArticles = () => {
+	const [articles, setArticles] = useState([]);
+
+	const fetchArticles = async () => {
+		const variables = { page: 0 };
+		const data = await fetch('https://api.hashnode.com/', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				query: GET_ARTICLES,
+				variables,
+			}),
+		});
+		const res = await data.json();
+		setArticles(res.data.user.publication.posts);
+	};
+
+	useEffect(() => {
+		fetchArticles();
+	}, []);
+
+	return (
+		<section className='bg-white text-black w-full'>
+			<div className='mx-auto max-w-screen-2xl px-4 py-32 lg:flex lg:items-center mt-12'>
+				<div className='sm:pl-24'>
+					<span className='bg-gradient-to-r from-sky-500 to-indigo-500 bg-clip-text text-transparent text-[20px] font-neueMontrealRegular font-semibold tracking-widest'>
+						[ browse articles ]
+					</span>
+					<div class='mt-4 text-[#020202] text-4xl sm:text-[54px] font-neueMontreal font-extrabold leading-[54px]'>
+						Explore the Latest Insights
+					</div>
+					<div className='mt-16 flex flex-col sm:flex-row gap-16 justify-center items-center'>
+						{articles.slice(0, 3).map((article, index) => (
+							<BlogCard
+								key={index}
+								coverImage={article.coverImage}
+								title={article.title}
+								brief={article.brief}
+								slug={article.slug}
+							/>
+						))}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default BrowseArticles;
